13f7541c086d9a2062677ca6ba4095fa4852a22c
kh-thien <khacthien.brand@gmail.com>
2026-02-01 14:27:32 +0700

    Initial Commit

---
+++ TodoAppForTestAssignment/TodoAppForTestAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppForTestAssignmentApp.swift
+//  TodoAppForTestAssignment
+//
+//  Created by NEIHT on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppForTestAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
